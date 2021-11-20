a783df6e5ea0a7e152413c35ac2249b58121405c
Nguyen Tien Nam <namnt2705>
2021-11-20 19:56:11 +0700

    [AndroiApp][TienNam] Edit some properties.

--- app/src/main/java/com/example/android_validate/MainActivity.java
+++ app/src/main/java/com/example/android_validate/MainActivity.java
@@ -3,12 +3,12 @@ package com.example.android_validate;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.EditText;
 import android.widget.Button;
 import android.widget.Toast;
-import android.view.View;
 import android.os.Bundle;
+import android.view.View;
 
 import java.util.regex.Pattern;
 import java.util.ArrayList;
@@ -16,22 +16,22 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    Spinner elType;
-    EditText elBedRoom;
-    RadioGroup elFurniture;
-    EditText elPriceByMonth;
-    EditText elReporterName;
-    EditText elCreatedAt;
-    Button elBtnSubmit;
+    Spinner type;
+    EditText bedRoom;
+    RadioGroup furniture;
+    EditText price;
+    EditText nameReporter;
+    EditText created_at;
+    Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        elBtnSubmit = findViewById(R.id.button_submit);
+        btnSubmit = findViewById(R.id.button_submit);
 
-        elBtnSubmit.setOnClickListener(new View.OnClickListener() {
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String[] arrValidate = {};
@@ -43,35 +43,35 @@ public class MainActivity extends AppCompatActivity {
                 List<String> types = new ArrayList<>(Arrays.asList(sampleTypes));
                 List<String> furnitures = new ArrayList<>(Arrays.asList(sampleFurnitures));
 
-                elType = findViewById(R.id.type);
-                String valType = elType.getSelectedItem().toString();
+                type = findViewById(R.id.type);
+                String valType = type.getSelectedItem().toString();
 
-                elBedRoom = findViewById(R.id.bedRoom);
-                String valBedRoom = elBedRoom.getText().toString();
+                bedRoom = findViewById(R.id.bedRoom);
+                String valBedRoom = bedRoom.getText().toString();
 
-                elFurniture = findViewById(R.id.furniture);
+                furniture = findViewById(R.id.furniture);
                 String valFurniture;
-                if (elFurniture.getCheckedRadioButtonId() != -1) {
-                    valFurniture = ((RadioButton)findViewById(elFurniture.getCheckedRadioButtonId())).getText().toString();
+                if (furniture.getCheckedRadioButtonId() != -1) {
+                    valFurniture = ((RadioButton)findViewById(furniture.getCheckedRadioButtonId())).getText().toString();
                 } else {
                     valFurniture = "";
                 }
 
-                elPriceByMonth = findViewById(R.id.priceByMonth);
-                String valPriceByMonth = elPriceByMonth.getText().toString();
+                price = findViewById(R.id.priceByMonth);
+                String valPriceByMonth = price.getText().toString();
 
-                elReporterName = findViewById(R.id.reporterName);
-                String valReporterName = elReporterName.getText().toString();
+                nameReporter = findViewById(R.id.reporterName);
+                String valReporterName = nameReporter.getText().toString();
 
-                elCreatedAt = findViewById(R.id.createdAt);
-                String valCreatedAt = elCreatedAt.getText().toString();
-
-                if (!(types.contains(valType))) {
-                    errors.add("Type");
-                }
+                created_at = findViewById(R.id.createdAt);
+                String valCreatedAt = created_at.getText().toString();
 
                 if (!(isNumber(valBedRoom))) {
-                    errors.add("Bed Room");
+                    errors.add("bed room");
+                }
+
+                if (!(types.contains(valType))) {
+                    errors.add("type");
                 }
 
                 if (!(furnitures.contains(valFurniture))) {
@@ -79,48 +79,48 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 if (!(isNumber(valPriceByMonth))) {
-                    errors.add("Price By Month");
+                    errors.add("Price");
                 }
 
                 if (isBlank(valReporterName)) {
-                    errors.add("Reporter Name");
+                    errors.add("Name of the reporter");
                 }
 
                 if (!(formatDate(valCreatedAt))) {
-                    errors.add("Created At");
+                    errors.add("Created_at");
                 }
 
                 if (errors.size() > 0) {
-                    Toast toast = Toast.makeText(MainActivity.this, sendErrorsMessage(errors), Toast.LENGTH_LONG);
+                    Toast toast = Toast.makeText(MainActivity.this, createMessageContent(errors), Toast.LENGTH_LONG);
                     toast.show();
                 } else {
-                    Toast toast = Toast.makeText(MainActivity.this, "You have successfully created the form!", Toast.LENGTH_LONG);
+                    Toast toast = Toast.makeText(MainActivity.this, "Created", Toast.LENGTH_LONG);
                     toast.show();
                 }
             }
         });
     }
 
-    public final static String sendErrorsMessage(List errors) {
+    public final static String createMessageContent(List errors) {
         String message = "";
         for (int error = 0; error < errors.size(); error++) {
             message += (errors.get(error) + ", ");
         }
 
-        return "Invalid " + message + "fields.";
+        return "Invalid data: " + message ", ";
     }
 
-    public final static boolean formatDate(String val)
-    {
-        return Pattern.compile("^\\d{1,2}[\\/.]\\d{1,2}[\\/.]\\d{4}$").matcher(val).matches();
-    }
-
-    public final static boolean isBlank(String val)
+    public final static boolean isBlank(String val) //valid for reporter name
     {
         return Pattern.compile("^\\s*$").matcher(val).matches();
     }
 
-    public final static boolean isNumber(String val)
+    public final static boolean isDate(String val) // DD-MM-YYYY
+    {
+        return Pattern.compile("^\\d{1,2}[\\/.]\\d{1,2}[\\/.]\\d{4}$").matcher(val).matches();
+    }
+
+    public final static boolean isNumber(String val) // valid for price and bedroom
     {
         return Pattern.compile("^\\d+$").matcher(val).matches();
     }
